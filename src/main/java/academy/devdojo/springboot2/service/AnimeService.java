2f0fff3d6620d48ff9e67a5c3447929cc4ee6b93
Adroaldo <hygino@msn.com>
2021-08-24 10:53:03 -0300

    Spring Boot 2 Essentials 09 - Método POST

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springboot2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,11 @@ import academy.devdojo.springboot2.domain.Anime;
 public class AnimeService {
 	// private final AnimeRepository animeRepository;
 
-	private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Naruto"));
+	private static List<Anime> animes;
+
+	static {
+		animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Naruto")));
+	}
 
 	public List<Anime> listAll() {
 		return animes;
@@ -22,4 +28,11 @@ public class AnimeService {
 		return animes.stream().filter(anime -> anime.getId().equals(id)).findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime id not found"));
 	}
+
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+		animes.add(anime);
+		return anime;
+	}
+
 }
