12a5a75c53fd0d0f0f9695cc5ff014a262f888d5
Adroaldo <hygino@msn.com>
2021-09-02 17:06:41 -0300

    BR 9+ Spring Boot 2 Essentials 16 - Request Params

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,11 @@
 package academy.devdojo.springboot2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import academy.devdojo.springboot2.domain.Anime;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long>{
-
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+	List<Anime> findByName(String name);
 }
